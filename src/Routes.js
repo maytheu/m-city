7720cf987406c3c93b239c1b735655f60bfe0928
Laue <maytheu98@gmail.com>
2019-01-01 10:05:14 +0100

    initial commit

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from './hoc/Layout';
+
+const Routes = () => {
+  return (
+    <div>
+      <Layout>
+        Header
+      </Layout>
+    </div>
+  );
+};
+
+export default Routes;
